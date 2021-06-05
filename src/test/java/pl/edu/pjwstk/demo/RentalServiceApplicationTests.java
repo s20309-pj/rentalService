6027cedb0a59ddaaa8604674c62e88f55e7dff47
Matyliano <joanna.boszko@gmail.com>
2021-06-05 14:10:17 +0200

    initial commit

---
+++ src/test/java/pl/edu/pjwstk/demo/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
